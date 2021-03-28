2dd50d25c4ea059269fed4f60ac47a41fb1bc8a6
HELENTRISHA <helen-trisha_student2021@wilder.school>
2021-03-28 18:01:37 +0200

    finish excersizeThree

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const replaceText= document.getElementById("replace-text")
 replaceText.style.color = "#777";
 replaceText.style.fontSize = "2rem";
 replaceText.innerHTML = "I can do <em>whatever</em> I want with JavaScript."
+
+const blueElements = document.getElementsByClassName("blue");
+
+for (let i = 0 ; i < blueElements.length; i++) {
+    blueElements[i].style.color = "blue";
+}
