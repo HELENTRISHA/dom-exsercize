cb56a951b477aa74d1b990acad851aa48ef86d89
HELENTRISHA <helen-trisha_student2021@wilder.school>
2021-03-28 18:29:35 +0200

    finish excersize four

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ const blueElements = document.getElementsByClassName("blue");
 for (let i = 0 ; i < blueElements.length; i++) {
     blueElements[i].style.color = "blue";
 }
+const openModal  = document.getElementById("open-modal");
+const closeModal = document.getElementById("close-modal");
+const modal = document.getElementById("modal");
+openModal.addEventListener("click", () =>{
+    modal.style.display = "block";
+});
+closeModal.addEventListener("click", () =>{
+    modal.style.display = "none";
+});
